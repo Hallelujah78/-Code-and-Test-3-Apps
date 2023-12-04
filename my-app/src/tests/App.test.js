695c8bc55cb68c3bda8504cb330d4a4dd415fa3b
winklevi1 <gavan.browne@gmail.com>
2023-12-04 14:08:58 +0000

    Updated App.test.js.
    Finished logo test and added link test, both passing.

--- my-app/src/tests/App.test.js
+++ my-app/src/tests/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
-// const { render, screen } = require('@testing-library/react');
 
 import App from '../App';
+import logo from  '../assets/images/logo.svg';
 
 test('a logo is rendered with the correct source', () => {
   render(<App />);
+  
+  const logoDOM = screen.getByRole('img', {name: /logo/i});
+  expect(logoDOM).toBeInTheDocument();
+  expect(logoDOM).toHaveAttribute('src', logo.src);
 });
+
+test('a link is displayed with the correct href', ()=>{
+render(<App/>);
+const link = screen.getByRole('link', {name: /learn react/i});
+expect(link).toBeInTheDocument();
+expect(link).toHaveAttribute('href', "https://react.dev/")
+})
